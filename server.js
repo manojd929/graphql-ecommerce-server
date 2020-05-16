e8c468de19e33af2c492f9823c8545fe5e50d013
Manoj Dinesh <manoj.d.2929@gmail.com>
2020-05-17 01:09:21 +0530

    graphql init0

---
+++ server.js
@@ -0,0 +1,45 @@
+const { GraphQLServer } = require('graphql-yoga')
+const db = require('./database')
+
+const typeDefs = `
+    type Query {
+        getProductsList: [Product]!,
+        getProductDetails(id: String!): Product!,
+        getBrandsList: [Brand]!
+        getBrandDetails(id: String!): Brand
+    }
+
+    type Product {
+        name: String!,
+        brandId: String!,
+        description: String,
+        availability: Int!,
+        price: Price!
+    }
+
+    type Price {
+        currency: String!,
+        value: Int!
+    }
+
+    type Brand {
+        name: String!
+    }
+`
+const resolvers = {
+    Query: {
+        getProductsList: () => db.products.list(),
+        getProductDetails: (root, { id }, context) => {
+            return db.products.get(id)
+        },
+        getBrandsList: () => db.brands.list(),
+        getBrandDetails: (root, { id }, context) => db.brands.get(id)
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => console.log('Server listening at 4000'))
